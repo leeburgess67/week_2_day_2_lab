2b9938bf92a32ad2ea826b21878d2cc28992aa90
leeburgess67 <leeburgessmail@gmail.com>
2018-05-29 13:54:25 +0100

    Initial commit

---
+++ specs/passenger_spec.rb
@@ -0,0 +1,25 @@
+require ("minitest/autorun")
+require_relative ("../passenger.rb")
+require ("minitest/rg")
+
+class PassengerTest < MiniTest::Test
+
+  def setup
+    passsenger1 = Passenger.new("Bob", 19)
+    passsenger2 = Passenger.new("Sam", 49)
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
